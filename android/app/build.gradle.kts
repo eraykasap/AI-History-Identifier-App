a8ddd12a660650e1dc1a6d52b9aca687c434505f
eraykasap <kasap.eray553@gmail.com>
2026-05-04 17:51:12 +0300

    list

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 78
-        versionName = "1.4.9"
+        versionCode = 79
+        versionName = "1.5.1"
     }
 
     signingConfigs {
